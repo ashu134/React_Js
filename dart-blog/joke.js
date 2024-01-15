11d14fbd9c53d86fdf976a5a335a3293f0f0f924
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2024-01-15 12:31:50 +0530

    new templete push

---
+++ dart-blog/joke.js
@@ -0,0 +1,12 @@
+[
+  "What falls, but never needs a bandage? The rain.",
+  "I was going to tell you a joke about boxing but I forgot the punch line.",
+  "I'm not a fan of spring cleaning. Let's be honest, I'm not into summer, fall, or winter cleaning either.",
+  "Why did the egg hide? It was a little chicken.",
+  "What did the dirt say to the rain? If you keep this up, my name will be mud!",
+  "Why couldn't the sunflower ride its bike? It lost its petals.",
+  "What's an egg's favorite vacation spot? New Yolk City.",
+  "I ate a sock yesterday. It was very time-consuming.",
+  "What kind of candy do astronauts like? Mars bars.",
+  "I wanted to buy some camo pants but couldn't find any.",
+];
