209e40d4eb785a5f7c7c865c0dbeb380eaea2935
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2024-01-30 23:34:39 +0530

    first commit

---
+++ HTML/myfirstscript.js
@@ -0,0 +1,9 @@
+console.log("This is my first javascript");
+
+for (var i = 0; i < 10; i++) {
+  console.log("okay");
+}
+// console.log(document.body.firstElementChild);
+console.log(document.getElementsByClassName);
+console.log(document.querySelectorAll("a"));
+console.log(document.getElementsByName("viewport"));
